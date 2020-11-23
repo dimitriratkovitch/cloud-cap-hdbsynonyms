f7bdc924e71c432da8006903a14f16ef023e86f9
elgordito123 <dimitri.ratkovitch@neomatic.io>
2020-11-23 21:01:19 +0100

    fix

--- srv/server.js
+++ srv/server.js
@@ -1,48 +1,8 @@
-/* eslint-disable capitalized-comments */
+const cds = require("@sap/cds");
 
-module.exports = async () => {
-  const app = require("express")();
-  const cds = require("@sap/cds");
-  const bodyParser = require("body-parser");
+cds.on("bootstrap", async (app) => {
+  await cds.mtx.in(app); // serve cds-mtx APIs
+});
 
-  //const cfenv = require("cfenv");
-  //const appEnv = cfenv.getAppEnv();
-
-  const xsenv = require("@sap/xsenv");
-  const services = xsenv.getServices({
-    uaa: { tag: "xsuaa" },
-    //  registry: { tag: "SaaS" },
-  });
-
-  const xssec = require("@sap/xssec");
-  const passport = require("passport");
-  passport.use("JWT", new xssec.JWTStrategy(services.uaa));
-  app.use(passport.initialize());
-  app.use(
-    passport.authenticate("JWT", {
-      session: false,
-    })
-  );
-
-  app.use(bodyParser.json());
-
-  // connect to datasource 'db' which must be the HANA instance manager
-  cds.connect.to("db");
-
-  cds.mtx.in(app).then(async () => {
-    console.log("XXX_Overriding Default Provisioning... ");
-    const provisioning = await cds.connect.to("ProvisioningService");
-    provisioning.impl(require("./handlers/provisioning"));
-  });
-
-  // serve application defined services: in combination with a CAP Java server, this won't appear here.
-  cds.serve("all").in(app);
-
-  //Start the Server
-  const port = process.env.PORT || 4004;
-  const server = app.listen(port, function () {
-    console.info(`app is listening at ${port}`);
-  });
-
-  return app;
-};
+// Delegate bootstrapping to built-in server.js
+module.exports = cds.server;
